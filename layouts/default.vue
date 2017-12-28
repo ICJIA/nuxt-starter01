8ce3fd1a728d8a1563a9182239b46be396e34f9d
Chris Schweda <cschweda@gmail.com>
2017-12-28 10:34:59 -0600

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+
+
+    <v-app id="inspire">
+      <v-navigation-drawer clipped fixed v-model="drawer" app>
+        <v-list dense style="margin-top: 30px">
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Dashboard</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>settings</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Settings</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+
+      </v-navigation-drawer>
+      <v-toolbar app fixed clipped-left>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-toolbar>
+      <v-content class="grey lighten-2">
+        <v-container fluid>
+          <nuxt/>
+        </v-container>
+
+      </v-content>
+      <v-footer>
+        <span>&copy; 2017</span>
+      </v-footer>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false
+    }),
+    props: {
+      source: String
+    }
+  }
+
+</script>
